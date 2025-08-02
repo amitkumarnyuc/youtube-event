f3faab45950cdbcfccedc0edc55bca5eb5e14356
Shreya <dev12@ramfincorp.com>
2025-08-02 18:35:13 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center gap-6 flex-col min-h-screen">
+      <h1 className="text-3xl text-center font-bold ">
+        React & Tailwind CSS Starter Pack
+      </h1>
+      <p className="text-center text-xl">
+        This is a starter pack for React & Tailwind CSS projects.
+      </p>
+      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
+    </main>
+  );
+}
+
+export default App;
