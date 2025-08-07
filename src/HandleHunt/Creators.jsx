66581ae41384683c3635f980e16d6f4ae43f85ec
Shreya <dev12@ramfincorp.com>
2025-08-07 10:38:41 +0530

    updated speed

--- src/HandleHunt/Creators.jsx
+++ src/HandleHunt/Creators.jsx
@@ -107,8 +107,8 @@ function Creator({ handleClick, data, handleBack, handleHome, Category }) {
 
                       initial={{ opacity: 0, scale: 0.95 }}
   animate={{ opacity: 1, scale: 1 }}
-  exit={{ opacity: 0, scale: 0.95 }}
-  transition={{ duration: 0.8, ease: "easeInOut" }}
+  exit={{ opacity: 0, scale: 0.85 }}
+  transition={{ duration: 0.4, ease: "easeInOut" }}
                     className="w-full px-2"
                   >
                    <Button
